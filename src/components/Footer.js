2466e82ba7838c8d53462589f91474356b9784b0
Philip Richert <prichert93@web.de>
2022-06-27 16:42:47 +0200

    Footer HTML added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export default function Footer() {
-	return <div></div>;
+
+
+
+
+	return <div>
+			<ul>
+				<li>Guideines</li>
+				<li>FAQ</li>
+				<li>Lists</li>
+				<li>API</li>
+				<li>Security</li>
+				<li>Legal</li>
+				<li>Apply to YC</li>
+				<li>Contact</li>
+			</ul>
+
+
+	</div>;
 }
